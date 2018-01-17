b353830d7dc0a17e19d3e26411b5c12bc813277b
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2018-01-17 17:27:54 +0100

    Added radamesh_param file to generate radamesh parameter files

---
+++ Generic_funcs/radamesh_params.rb
@@ -0,0 +1,34 @@
+# Generating RADAMESH parameter file
+class RadameshParams
+  def initialize
+    @blocks = {
+      runtime: {},
+      species: {},
+      snapshot: {},
+      cosmology: {},
+      background: {},
+      sources: {},
+      ray_tracing: {},
+      output: {},
+    }
+  end
+
+  def set_section(section, params)
+    return unless params.is_a?(Hash)
+    @blocks[section.to_sym] = params
+  end
+
+  def set(section, k, v)
+    @blocks[section.to_sym][k.to_s] = v.to_s
+  end
+
+  def write(path, blocks=@blocks)
+    File.open(path, 'w') do |f|
+      blocks.each do |section, params|
+        f.write("##{section}\n")
+        params.each { |k, v| f.write("#{k.to_s}=#{v.to_s}\n") }
+        f.write("\n")
+      end
+    end
+  end
+end
