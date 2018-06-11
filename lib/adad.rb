e295b3efd0e39e711dfd7f89c48ffaa318af9d0d
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2018-06-11 15:12:04 +0200

    Upgraded adad module

--- lib/adad.rb
+++ lib/adad.rb
@@ -1,332 +1,285 @@
-module Adad
-  class Generate
-
-    # Initialize a number with its unit
-    #
-    # ==== Attributes
-    # +value+:: value of the number
-    # +epsilon+:: absolute uncertainty(-ies) of the value
-    # +units+:: units and their powers, e.g. :kg, 1, :m, 1, :s, -2
-    #
-    # ==== Examples
-    # > H = Adad.new 67.48, [0.98], :km, 1, :Mpc, -1, :s, -1
-    # => #<Adad::Generate:...>
-    def initialize(value, epsilon=nil, *units_and_powers)
-      @A = { v: value, u: { L: [], M: [], T: [], Th: [], N: [] }, e: [0, 0]}
-
-      unless epsilon.nil?
-        case epsilon.length
-        when 0
-          @A[:e] = [0, 0]
-        when 1
-          @A[:e] = [epsilon[0], epsilon[0]]
-        when 2
-          @A[:e] = epsilon
-        else
-          raise ArgumentError, "Don't know how to handle uncertainties"
-        end
-      end
-
-      units_and_powers.each_slice(2) do |unit, pow|
-        prfx, symb = extract_prfx unit
-
-        Dim.each do |dim|
-          next unless U[dim].key? symb
-          @A[:u][dim] << Adad::gen_unit(prfx, symb, pow)
-        end
-
-        DU[symb].each do |d,us|
-          us.each do |u|
-            @A[:u][d] << Adad::gen_unit(u[:prfx], u[:symb], u[:pow] * pow)
-          end
-        end if DU.key? symb
-      end
-    end
-
-
-    # Returns the value of the number
-    def value
-      @A[:v]
-    end
-
-    def v
-      self.value()
-    end
-
-
-    # Returns the unit of the number
-    def unit
-      unit = "["
-      @A[:u].each do |_,us|
-        us.each do |u|
-          unit += " #{u[:prfx] unless u[:prfx] == :one}#{u[:symb]}"
-          unit += "^#{u[:pow]}" unless u[:pow] == 1
-        end
-      end
-      unit += " ]"
-    end
-
-    def u
-      self.unit()
-    end
-
-
-    # Uncertainties
-    def e
-      @A[:e]
-    end
-
-
-    # Cloning
-    def clone
-      adad = Adad.new @A[:v]
-      adad.instance_variable_get(:@A)[:u] = Marshal.load(Marshal.dump(@A[:u]))
-      adad.instance_variable_get(:@A)[:e] = Marshal.load(Marshal.dump(@A[:e]))
-      return adad
-    end
-
-
-    # Convert the unit of the number to a given unit
-    def to(*units_and_powers)
-      # TODO: Check if dimensions match
-
-      adad, a = _clone_self
-
-      remove_units(a)
-
-      units_and_powers.each_slice(2) do |unit, pow|
-        prfx, symb = extract_prfx unit
-
-        Dim.each do |dim|
-          next unless U[dim].key? symb
-
-          u = Adad::gen_unit(prfx, symb, pow)
-
-          a[:v] /= conv_fact(u)
-          a[:u][dim] << u
-        end
-
-        DU[symb].each do |d, us|
-          us.each do |u|
-            a[:v] /= conv_fact(u)
-            a[:u][d] << Adad::gen_unit(u[:prfx], u[:symb], u[:pow] * pow)
-          end
-        end if DU.key? symb
-      end
-
-      return adad
-    end
-
-
-    # Simplify units by canceling similar dimensions (if present)
-    def simplify!()
-      Dim.each do |dim|
-        next if @A[:u][dim].length <= 1
-
-        sum_pows = @A[:u][dim].map { |u| u[:pow] }.reduce(:+)
-        @A[:u][dim].each { |u| @A[:v] *= conv_fact(u) }
-
-        if sum_pows == 0
-          @A[:u][dim] = []
-        else
-          def_u = U[dim][:default]
-          @A[:u][dim] = [Adad::gen_unit(def_u[:prfx], def_u[:symb], sum_pows)]
-          @A[:v] /= conv_fact(def_u)
-        end
-      end
-
-      return @A[:u]
-    end
-
-
-    def same_dimension?(us)
-      Dim.each { |d| return false unless tot_pow(@A[:u][d]) == tot_pow(us[d]) }
-    end
-
-
-    # Addition
-    def +(m, mod=1)
-      raise ArgumentError unless m.is_a? Adad::Generate
-
-      adad, a = _clone_self
-      m_a = m.instance_variable_get(:@A)
-
-      raise ArgumentError unless self.same_dimension? m_a[:u]
-
-      a[:v] += m_a[:v] * _to_SI(m_a[:u]) / _to_SI(@A[:u]) * mod
-
-      return adad
-    end
-
-    def -(m)
-      self.+(m, -1)
-    end
-
-
-    # Multiplying with an Adad instance or a scalar
-    # TODO: using coerce to be able to run scalar * Adad
-    def *(m, mod=1)
-      adad, a = _clone_self
-
-      if m.is_a? (Numeric)
-        a[:v] *= m**mod
-        [0,1].each { |i| a[:e][i] *= m }
-      elsif m.is_a? (Adad::Generate)
-        m_A = m.instance_variable_get(:@A)
-
-        [0,1].each { |i| a[:e][i] = m_A[:v] * a[:e][i] + a[:v] * m_A[:e][i] }
-
-        a[:v] *= m_A[:v]**mod
-
-        m_A[:u].each do |d, m_us|
-          m_us.each do |m_u|
-            a[:u][d] << Adad::gen_unit(m_u[:prfx], m_u[:symb], m_u[:pow] * mod)
-          end
-        end
-      end
-
-      return adad
-    end
-
-
-    # Dividing by an Adad instance or a scalar
-    def /(m)
-      self.*(m, -1)
-    end
-
-
-    def **(m)
-      raise ArgumentError unless m.is_a? (Numeric)
-
-      adad, a = _clone_self
-
-      a **= m
-      a[:u].each { |_, us| us.each { |u| u[:pow] *= m } }
-      [0,1].each { |i| a[:e][i] *= m }
-
-      return adad
-    end
-
-
-    private
-    def extract_prfx(unit)
-      return :one, unit if DU.key? unit
-
-      Dim.each { |dim| return :one, unit if U[dim].key? unit }
-
-      PRFX.keys.each do |p|
-        u = unit.to_s
-        if u.start_with? p.to_s
-          u[p.to_s] = ''
-          return p, u.to_sym
-        end
-      end
-    end
-
-
-    # Returns the SI conversion factor
-    def _to_SI(units)
-      units.map { |_,us| us.map { |u| conv_fact(u) } }.flatten.reduce :*
-    end
-
-
-    def _clone_self
-      adad = self.clone
-      return adad, adad.instance_variable_get(:@A)
-    end
-
-
-    def tot_pow(units)
-      return units.map { |u| u[:pow] }.reduce(:+) || 0
-    end
-
-
-    def conv_fact(unit)
-      f = U.values.select{ |u| u.key? unit[:symb]}.first
-      railse ArgumentError, "Unknown unit: #{unit[:symb]}" unless f
-      (PRFX[unit[:prfx]] * f[unit[:symb]][:conv])**unit[:pow]
-    end
-
-
-    def remove_units(a)
-      Dim.each do |dim|
-        a[:u][dim].each { |u| a[:v] *= conv_fact(u) }
-        a[:u][dim] = []
-      end
-    end
-  end
-
-
-  def self.new(value, epsilon=nil, *units)
+require_relative './unit.rb'
+
+class Adad
+
+  # Initializing an instance of Adad
+  #
+  # === Attributes
+  # +value+:: value of the new Adad
+  # +epsilon+:: absolute uncertainty(-ies_ of the Adad) (optional)
+  # +units_and_powers+:: pairs of units and their powers in succession
+  #
+  # ==== Examples
+  # > H = Adad.new 67.48, [0.98], :km, 1, :Mpc, -1, :s, -1
+  # => #<Adad:...>
+  def initialize(value, epsilon=nil, *units_and_powers)
     if epsilon.is_a?(Symbol)
-      Generate.new value, nil, epsilon, *units
-    else
-      Generate.new value, epsilon, *units
+      return initialize(value, nil, epsilon, *units_and_powers)
+    end
+
+    @A = { v: value, u: { L: [], M: [], T: [], Th: [], N: [] }, e: [0.0, 0.0]}
+    @symb = nil
+
+    unless epsilon.nil?
+      case epsilon.length
+      when 0
+        @A[:e] = [0, 0]
+      when 1
+        @A[:e] = [epsilon[0], epsilon[0]]
+      when 2
+        @A[:e] = epsilon
+      else
+        raise ArgumentError, "Don't know how to handle uncertainties"
+      end
+    end
+
+    units_and_powers.each_slice(2) do |unit, pow|
+      prfx, symb = Unit::check_for_prfx unit
+
+      Unit::Dim.each do |dim|
+        next unless Unit::UNITS[dim].key? symb
+        @A[:u][dim] << Unit::gen_unit(prfx, symb, pow)
+      end
+
+      Unit::DERIVED[symb].each do |d,us|
+        us.each do |u|
+          @A[:u][d] << Unit::gen_unit(u[:prfx], u[:symb], u[:pow] * pow)
+        end
+      end if Unit::DERIVED.key? symb
     end
   end
 
 
-  def self.gen_unit(prfx, symb, pow)
-    { prfx: prfx, symb: symb, pow: pow}
+  # Returns the value of the number
+  def value
+    @A[:v]
+  end
+
+  def v
+    self.value()
   end
 
 
-  U = { # Units
-    L: {
-      default: gen_unit(:one, :m, 1),
-      m: { conv: 1 },
-      pc: { conv: 3.086e16 },
-    },
-    M: {
-      default: gen_unit(:k, :g, 1),
-      g: { conv: 0.001 },
-      Msun: { conv: 1.989e30 },
-    },
-    T: {
-      default: gen_unit(:one, :s, 1),
-      s: { conv: 1 },
-      yr: { conv: 3.154e7 },
-    },
-    Th: {
-      default: gen_unit(:one, :K, 1),
-      K: { conv: 1 },
-    },
-    N: {
-      default: gen_unit(:one, :mol, 1),
-      mol: { conv: 1},
-    }
-  }
+  # Returns the unit of the number
+  def unit
+    unit = "["
+    @A[:u].each do |_,us|
+      us.each do |u|
+        unit += " #{u[:prfx] unless u[:prfx] == :one}#{u[:symb]}"
+        unit += "^#{u[:pow]}" unless u[:pow] == 1
+      end
+    end
+    unit += " ]"
+  end
 
-  Dim = U.keys # Dimension Symbols
+  def u
+    self.unit()
+  end
 
 
-  DU = { # Derived Units
-    Hz: { T: [gen_unit(:one, :s, -1)] },
-    J: {
-      M: [gen_unit(:k, :g, 1)],
-      L: [gen_unit(:one, :m, 2)],
-      T: [gen_unit(:one, :s, -2)]
-    },
-    N: {
-      M: [gen_unit(:k, :g, 1)],
-      L: [gen_unit(:one, :m, 1)],
-      T: [gen_unit(:one, :s, -2)]
-    },
-    Pa: {
-      M: [gen_unit(:k, :g, 1)],
-      L: [gen_unit(:one, :m, -1)],
-      T: [gen_unit(:one, :s, -2)]
-    },
-    W: {
-      M: [gen_unit(:k, :g, 1)],
-      L: [gen_unit(:one, :m, 2)],
-      T: [gen_unit(:one, :s, -3)]
-    }
-  }
+  # Returns uncertainties
+  def e
+    @A[:e]
+  end
 
 
-  PRFX = { # Prefixes
-    E: 1e18, P: 1e15, T: 1e12, G: 1e9, M: 1e6, k: 1e3, h: 1e2, one: 1,
-    d: 1e-1, c: 1e-2, m: 1e-3, u: 1e-6, n: 1e-9, p: 1e-12, f: 1e-15, a: 1e-18
-  }
+  # Cloning
+  def clone
+    adad = Adad.new @A[:v]
+    adad.instance_variable_get(:@A)[:u] = Marshal.load(Marshal.dump(@A[:u]))
+    adad.instance_variable_get(:@A)[:e] = Marshal.load(Marshal.dump(@A[:e]))
+    return adad
+  end
+
+
+  # Check if a given unit have the same dimension of the self unit
+  #
+  # === Attributes
+  # +adad+:: a given Adad
+  def same_dimension?(adad)
+    def pows(us)
+      return us.map { |u| u[:pow] }.reduce(:+) || 0
+    end
+
+    us = adad.instance_variable_get(:@A)[:u]
+    Unit::Dim.each { |d| return false unless pows(@A[:u][d]) == pows(us[d]) }
+
+    return true
+  end
+
+
+  # Addition
+  #
+  # === Attributes
+  # +s+:: a given summand of type Adad
+  def +(s, mod=1)
+    raise ArgumentError unless s.is_a? Adad
+    raise ArgumentError unless self.same_dimension? s
+
+    adad, a = _clone_self
+    s_a = s.instance_variable_get(:@A)
+
+    conv_fact = _factor_to_SI(s) / _factor_to_SI(self)
+
+    a[:v] += s_a[:v] * conv_fact * mod
+    unless s_a[:u].empty?
+      [0,1].each { |i| a[:e][i] += conv_fact * s_a[:e][i] }
+    end
+
+    return adad
+  end
+
+  # Subtracting by an Adad
+  #
+  # === Attributes
+  # +s+:: a given subtrahend of type Adad
+  def -(s)
+    self.+(s, -1)
+  end
+
+
+  # Multiplying by an Adad or a Numeric value
+  #
+  # === Attributes
+  # +m+:: multiplicand of type Adad or Numeric
+  #
+  # TODO: use coerce to be able to run scalar * Adad
+  def *(m, mod=1)
+    adad, a = _clone_self
+
+    if m.is_a? (Numeric)
+      a[:v] *= m**mod
+      a[:e] = a[:e].collect { |e| e*m**mod }
+    elsif m.is_a? (Adad)
+      m_A = m.instance_variable_get(:@A)
+
+      [0,1].each { |i| a[:e][i] = m_A[:v] * a[:e][i] + a[:v] * m_A[:e][i] }
+
+      a[:v] *= m_A[:v]**mod
+
+      m_A[:u].each do |d, m_us|
+        m_us.each do |m_u|
+          a[:u][d] << Unit::gen_unit(m_u[:prfx], m_u[:symb], m_u[:pow] * mod)
+        end
+      end
+    end
+
+    return adad
+  end
+
+
+  # Division by an Adad or a Numeric value
+  #
+  # === Attributes
+  # +d+:: divisor of type Adad or Numeric
+  def /(d)
+    self.*(d, -1)
+  end
+
+
+  # Exponentiation by a scalar (of type Numeric)
+  #
+  # === Attributes
+  # +ex+:: exponent of type Numeric
+  def **(ex)
+    raise ArgumentError unless ex.is_a? (Numeric)
+
+    adad, a = _clone_self
+
+    a[:v] **= ex
+    a[:u].each { |_, us| us.each { |u| u[:pow] *= ex } }
+    [0,1].each { |i| a[:e][i] *= ex }
+
+    return adad
+  end
+
+
+  # Simplify units by canceling similar dimensions (if present)
+  def simplify!
+    Unit::Dim.each do |dim|
+      next if @A[:u][dim].length <= 1
+
+      sum_pows = @A[:u][dim].map { |u| u[:pow] }.reduce(:+)
+      @A[:u][dim].each { |u| @A[:v] *= Unit::conv_fact(u) }
+
+      if sum_pows == 0
+        @A[:u][dim] = []
+      else
+        def_u = Unit::UNITS[dim][:default]
+        @A[:u][dim] = [Unit::gen_unit(def_u[:prfx], def_u[:symb], sum_pows)]
+        @A[:v] /= Unit::conv_fact(def_u)
+      end
+    end
+
+    return self
+  end
+
+
+  # Returns a converted Adad with a new given unit
+  #
+  # === Attributes
+  # +units_and_powers+:: pairs of units and their powers in succession
+  # TODO: Check if dimensions match
+  def to(*units_and_powers)
+
+    adad, a = _clone_self
+
+    Unit::Dim.each do |dim|
+      a[:u][dim].each { |u| a[:v] *= Unit::conv_fact(u) }
+      a[:u][dim] = []
+    end
+
+    units_and_powers.each_slice(2) do |unit, pow|
+      prfx, symb = Unit::check_for_prfx unit
+
+      Unit::Dim.each do |dim|
+        next unless Unit::UNITS[dim].key? symb
+
+        u = Unit::gen_unit(prfx, symb, pow)
+
+        a[:v] /= Unit::conv_fact(u)
+        a[:u][dim] << u
+      end
+
+      Unit::DERIVED[symb].each do |d, us|
+        us.each do |u|
+          a[:v] /= Unit::conv_fact(u)
+          a[:u][d] << Unit::gen_unit(u[:prfx], u[:symb], u[:pow] * pow)
+        end
+      end if Unit::DERIVED.key? symb
+    end
+
+    return adad
+  end
+
+  # Generating a symbol version of the adad
+  def symb
+    return @symb unless @symb.nil?
+
+    @symb = Formul.new self
+    @symb
+  end
+
+  private
+
+  # Returns a cloned version of self alongside of pointer to the @A of the clone
+  def _clone_self
+    adad = self.clone
+    return adad, adad.instance_variable_get(:@A)
+  end
+
+  # Returns the SI conversion factor (only a Numeric)
+  #
+  # === Attributes
+  # +adad+:: a given adad
+  def _factor_to_SI(adad)
+    a = adad.instance_variable_get(:@A)
+    return 1.0 if a[:u].all? { |x| x[1].empty? }
+    adad.instance_variable_get(:@A)[:u].map { |_,us|
+      us.map { |u| Unit::conv_fact(u) }
+    }.flatten.reduce :*
+  end
+
+
 end
+
